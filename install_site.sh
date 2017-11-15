a6ee2c2c0fb9a3c6814c5e7a29b1d4a4a5fe8070
gavincmi <501554+g7morris@users.noreply.github.com>
2017-11-15 11:04:09 -0500

    Ahem. The real changes to install_site.sh

--- install_site.sh
+++ install_site.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+
 echo "Changing directory to /var/www/html"
-cd /var/www/html || exit
+cd /home/islandora/site || exit
 
 echo "Using Drush makefile to create sample Drupal site within /var/www/html"
-/usr/local/bin/drush make --prepare-install /home/islandora/site/drupal.drush.make /var/www/html
+/usr/local/bin/drush make --prepare-install /home/islandora/site/drupal.drush.make /home/islandora/site/drupal_install
 
 echo "Using Islandora makefile for Islandora Modules for sample Drupal site within /var/www/html"
-/usr/local/bin/drush make --no-core /home/islandora/site/islandora.drush.make /var/www/html
+/usr/local/bin/drush make --no-core /home/islandora/site/islandora.drush.make /var/www/html/drupal_install
+
+echo "Copy /home/islandora/site/drupal contents to /var/www/html"
+cp -Rv /home/islandora/site/drupal_install/. /var/www/html/sites/default/
 
 echo "Copy settings.php file for sample Site to work"
 cp -v /home/islandora/site/settings.php /var/www/html/sites/default/

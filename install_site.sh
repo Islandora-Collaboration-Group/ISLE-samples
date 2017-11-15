ee1814ab2291a915d0d4705490f1a9209b1d179b
gavincmi <501554+g7morris@users.noreply.github.com>
2017-11-15 11:36:07 -0500

    Broke out drush make process into separate script make_site.sh from install_site.sh

--- install_site.sh
+++ install_site.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-
-echo "Changing directory to /home/islandora/site"
-cd /home/islandora/site || exit
-
-echo "Using Drush makefile to create sample Drupal site within /home/islandora/site/drupal_install"
-/usr/local/bin/drush make --prepare-install /home/islandora/site/drupal.drush.make /home/islandora/site/drupal_install
-
-echo "Using Islandora makefile for Islandora Modules for sample Drupal site within /var/www/html/drupal_install"
-/usr/local/bin/drush make --no-core /home/islandora/site/islandora.drush.make /var/www/html/drupal_install
-
-echo "Copy /home/islandora/site/drupal_install contents to /var/www/html"
-cp -Rv /home/islandora/site/drupal_install/. /var/www/html/sites/default/
-
-echo "Copy settings.php file for sample Site to work"
-cp -v /home/islandora/site/settings.php /var/www/html/sites/default/
-
 echo "Installing all Islandora modules"
 cd /var/www/html/sites/all/modules || exit
 
